33b4a13dfb74488198d0a5c8ea5500ec5e6bb347
Ulinator <zahuta@gmail.com>
2020-10-12 10:24:12 +0200

    Updates

---
+++ precache-manifest.2ce25a49b368d1ed0044e3a23fbcf48b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c3d8ff1995f4f9a66fc2a178d184d3",
+    "url": "/sw-planets/index.html"
+  },
+  {
+    "revision": "ce301c3c807a6dcdf376",
+    "url": "/sw-planets/static/css/main.dee9cab5.chunk.css"
+  },
+  {
+    "revision": "3bd1e7e5747c5c1cf0b1",
+    "url": "/sw-planets/static/js/2.e5e6d99f.chunk.js"
+  },
+  {
+    "revision": "d86f3a7a1e3c7f8afbd22066d3fc3c72",
+    "url": "/sw-planets/static/js/2.e5e6d99f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce301c3c807a6dcdf376",
+    "url": "/sw-planets/static/js/main.d1501fdc.chunk.js"
+  },
+  {
+    "revision": "e6f1b24ba6886ce045ff",
+    "url": "/sw-planets/static/js/runtime-main.190cd6d9.js"
+  },
+  {
+    "revision": "81e948ad0179ac79be6c28982906dae1",
+    "url": "/sw-planets/static/media/ARROW_CLOSE.81e948ad.svg"
+  },
+  {
+    "revision": "da45c4c0de0efd2c6e1fd463b04d3b3f",
+    "url": "/sw-planets/static/media/ARROW_OPEN.da45c4c0.svg"
+  },
+  {
+    "revision": "4b823e924575020f741e171a79ab181d",
+    "url": "/sw-planets/static/media/CLOSE.4b823e92.svg"
+  },
+  {
+    "revision": "8bbd21d36c77b175dfaa28e43fcedf7a",
+    "url": "/sw-planets/static/media/LOADER.8bbd21d3.svg"
+  },
+  {
+    "revision": "79977f053d6f42e5e0575fa2c33b24cd",
+    "url": "/sw-planets/static/media/LOGO.79977f05.svg"
+  },
+  {
+    "revision": "69ce55028b71f39e2b9e10faaf614c5e",
+    "url": "/sw-planets/static/media/SORT_ARROW.69ce5502.svg"
+  }
+]);
